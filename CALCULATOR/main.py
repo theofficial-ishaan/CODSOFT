33e40256f0968db5c66d3250719a737b1f19e98d
Ishaan Mishra <officialishaan.m@gmail.com>
2025-07-17 15:28:11 +0530

    Refactor calculator function to improve input handling and error management

--- CALCULATOR/main.py
+++ CALCULATOR/main.py
@@ -2,18 +2,23 @@
 # Prompt the user to input two numbers and and an operator
 # Display the Result.
 
-try :
-    a = int(input("Enter 1st number: "))
-    b = int(input("Enter 2nd number: "))
-    op = input("Enter an operator(+,-,*,/,%): ")
+def calc():
+    try :
+        a = int(input("Enter 1st number: "))
+        b = int(input("Enter 2nd number: "))
+        op = input("Enter an operator(+,-,*,/,%): ")
 
-    match op:
-        case '+': print("Result:", a+b)
-        case '-': print("Result:", a-b)
-        case '*': print("Result:", a*b)
-        case '/': print("Result:", a/b)
-        case '%': print("Result:", a%b)
-        case _: print("Please enter valid operators!")
+        match op:
+            case '+': print("Result:", a+b)
+            case '-': print("Result:", a-b)
+            case '*': print("Result:", a*b)
+            case '/': 
+                try: print("Result:", a/b) 
+                except ZeroDivisionError: print("You cannot divide by zero!"); calc()
+            case '%': print("Result:", a%b)
+            case _: print("Please enter valid operators!"); calc()
 
-except ValueError:
-    print("Please enter numeric values only!!")
+    except ValueError:
+        print("Please enter numeric values only!!"); calc()
+   
+calc()
