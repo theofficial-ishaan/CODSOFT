ec5972cc2ea65808ae3d494781e1a4f134d35a97
Ishaan Mishra <officialishaan.m@gmail.com>
2025-07-17 15:19:19 +0530

    Refactor input handling and error management

--- CALCULATOR/main.py
+++ CALCULATOR/main.py
@@ -2,14 +2,18 @@
 # Prompt the user to input two numbers and and an operator
 # Display the Result.
 
-a = int(input("Enter 1st number: "))
-b = int(input("Enter 2nd number: "))
-op = input("Enter an operator(+,-,*,/,%): ")
+try :
+    a = int(input("Enter 1st number: "))
+    b = int(input("Enter 2nd number: "))
+    op = input("Enter an operator(+,-,*,/,%): ")
 
-match op:
-    case '+': print("Result:", a+b)
-    case '-': print("Result:", a-b)
-    case '*': print("Result:", a*b)
-    case '/': print("Result:", a/b)
-    case '%': print("Result:", a%b)
-    
+    match op:
+        case '+': print("Result:", a+b)
+        case '-': print("Result:", a-b)
+        case '*': print("Result:", a*b)
+        case '/': print("Result:", a/b)
+        case '%': print("Result:", a%b)
+        case _: print("Please enter valid operators!")
+
+except ValueError:
+    print("Please enter numeric values only!!")
